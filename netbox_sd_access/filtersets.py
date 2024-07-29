98eb7a94798a840ddf1a335a184b3ea25d3c1a81
Theo <62950302+t1an-xyz@users.noreply.github.com>
2024-07-29 18:24:31 +0000

    Documentation for filtersets, modify fields

--- netbox_sd_access/filtersets.py
+++ netbox_sd_access/filtersets.py
@@ -4,17 +4,11 @@ from netbox.filtersets import NetBoxModelFilterSet
 from .models import *
 from dcim.models import Site
 
-# class SDAccessFilterSet(NetBoxModelFilterSet):
-#
-#     class Meta:
-#         model = SDAccess
-#         fields = ['name', ]
-#
-#     def search(self, queryset, name, value):
-#         return queryset.filter(description__icontains=value)
-
 class FabricSiteFilterSet(NetBoxModelFilterSet):
-    
+    """
+    Filter set for Fabric Sites.
+    Allows filtering by ID, name, physical site, location, and IP Pool, as well as searching by name or comments.
+    """
     class Meta:
         model = FabricSite
         fields = ('id', 'name', 'physical_site', 'location', 'ip_prefixes', 'comments')
@@ -23,11 +17,15 @@ class FabricSiteFilterSet(NetBoxModelFilterSet):
         return queryset.filter(Q(name__icontains=value) | Q(comments__icontains=value))
 
 class SDADeviceFilterSet(NetBoxModelFilterSet):
+    """
+    Filter set for SDA Devices.
+    Allows filtering by ID, role, and fabric site, as well as searching by comments or device name.
+    """
     role = django_filters.CharFilter(field_name='role', lookup_expr='exact')
     
     class Meta:
         model = SDADevice
-        fields = ('role','fabric_site')
+        fields = ('id', 'role','fabric_site')
     
     def search(self, queryset, name, value):
         # return queryset.filter(comments__icontains=value)
@@ -35,14 +33,23 @@ class SDADeviceFilterSet(NetBoxModelFilterSet):
     
 
 class IPTransitFilterSet(NetBoxModelFilterSet):
+    """
+    Filter set for IP Transits.
+    Allows filtering by id, name, fabric site, ASN, and comments, as well as searching by name or comments.
+    """
     class Meta:
         model = IPTransit
         fields = ('id', 'name', 'fabric_site', 'asn', 'comments')
         
     def search(self, queryset, name, value):
-        return queryset.filter(name__icontains=value)
+        return queryset.filter(Q(name__icontains=value) | Q(comments__icontains=value))
 
 class SDATransitFilterSet(NetBoxModelFilterSet):
+    """
+    Filter set for SDA Transits.
+    Allows filtering by id, name, transit type, fabric site, control plane node, and devices 
+    as well as searching by name or comments.
+    """
     transit_type = django_filters.CharFilter(field_name='transit_type', lookup_expr='exact')
     
     class Meta:
@@ -50,9 +57,13 @@ class SDATransitFilterSet(NetBoxModelFilterSet):
         fields = ('id', 'name', 'transit_type', 'fabric_site', 'control_plane_node', 'devices', 'comments')
         
     def search(self, queryset, name, value):
-        return queryset.filter(name__icontains=value)
+        return queryset.filter(Q(name__icontains=value) | Q(comments__icontains=value))
 
 class IPPoolFilterSet(NetBoxModelFilterSet):
+    """
+    Filter set for IP Pools.
+    Allows filtering by ID, name, gateway, DHCP server, and DNS servers, as well as searching by name or comments.
+    """
     class Meta:
         model = IPPool
         fields = ('id', 'name', 'gateway', 'dhcp_server', 'dns_servers', 'comments')
@@ -61,10 +72,14 @@ class IPPoolFilterSet(NetBoxModelFilterSet):
         return queryset.filter(Q(name__icontains=value) | Q(comments__icontains=value))
     
 class VirtualNetworkFilterSet(NetBoxModelFilterSet):
+    """
+    Filter set for Virtual Networks.
+    Allows filtering by name, fabric site, and VRF, as well as searching by name.
+    """
 
     class Meta:
         model = VirtualNetwork
-        fields = ('name', 'fabric_site', 'vrf')
+        fields = ('id', 'name', 'fabric_site', 'vrf')
 
     def search(self, queryset, name, value):
         return queryset.filter(name_icontains=value)
